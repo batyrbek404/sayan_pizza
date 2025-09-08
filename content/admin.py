a7b8080f4cadbe18ec5b76c9f3125e15822ab03e
ayan584 <s98227636@gmail.com>
2025-09-08 13:36:37 +0600

    изменения в content

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Content
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'created_at')
+    list_filter = ('is_published', 'created_at')
+    search_fields = ('title', 'body')
+from django.contrib import admin
 
 # Register your models here.
--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
+from .models import Content
+from .serializers import ContentSerializer
+
+class ContentViewSet(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
 from django.shortcuts import render
 
 # Create your views here.
